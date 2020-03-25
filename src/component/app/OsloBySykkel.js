ead93eb647b4ab5b7bad1fe5a51a79ef1639e9fc
gurudutt <egurudutt@gmail.com>
2020-03-25 14:11:36 +0100

    - Kodeopprydding og klar for koding

---
+++ src/component/app/OsloBySykkel.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './OsloBySykkel.css';
+
+class OsloBySykkel extends Component {
+
+  render() {
+  return (
+    <div className="OsloBySykkel">
+      Oslo By sykkel
+    </div>
+  );
+  }
+}
+
+export default OsloBySykkel;
